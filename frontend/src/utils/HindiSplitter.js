1c9f928f8d66cf1a737db859cd93664352607f66
Rajvee Sheth <rajvee.sheth@iitgn.ac.in>
2025-07-24 22:48:37 +0530

    Delete frontend/src directory

--- frontend/src/utils/HindiSplitter.js
+++
@@ -1,9 +0,0 @@
-const HindiSplitter = (sentence="नमस्ते, यह शुभ है। यह एक एनोटेशन टूल है।") => {
-    let sent = sentence.replace(/,/g, "");
-    sent = sent.replace(/;/g, "");
-    sent = sent.replace(/।/g, "");
-
-    return sent.split(' ');
-};
-
-export default HindiSplitter;
